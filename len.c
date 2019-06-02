f52286295ce6cceed72caf40e9cf4a0648cd1fdf
Cobraxo <33578076+Cobraxo@users.noreply.github.com>
2019-06-02 12:28:59 +0200

    Update len.c

--- len.c
+++ len.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 
     int len = 0;
 
-    if (argc < 2) printf("Usage: %s string\n", argv[0]), exit(1);   //If there are no arguments, exit
+    if (argc < 2) printf("Usage: %s <string>\n", argv[0]), exit(1); //If there are no arguments, exit
     
     for (int i; argv[1][i] != '\0' ; i++, len++);                   //len = numbers of char in argv[1]
     printf("%d\n", len);
